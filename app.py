43334b4e08bc45344288dc2dbad7404fe0363edc
YanirNa <yanir274@hgmail.com>
2024-02-19 15:36:56 +0200

    scrape with description & other movies recommend func

---
+++ app.py
@@ -0,0 +1,41 @@
+# import tkinter as tk
+# from tkinter import *
+# from tkinter import messagebox
+# import pandas as pd
+
+
+# # Main Tkinter window
+# root = tk.Tk()
+# root.title("Movie Recommendation System")
+
+# # UI elements
+# Label(root, text="Genre:").pack()
+# genre_entry = Entry(root)
+# genre_entry.pack()
+
+# Label(root, text="Director:").pack()
+# director_entry = Entry(root)
+# director_entry.pack()
+
+# Label(root, text="Duration:").pack()
+# actor_entry = Entry(root)
+# actor_entry.pack()
+
+# Label(root, text="Rating:").pack()
+# actor_entry = Entry(root)
+# actor_entry.pack()
+
+# recommend_button = Button(root, text="Get Recommendations", command=get_recommendations)
+# recommend_button.pack()
+
+# # Display recommendations and winner movie
+# result_textbox = Text(root, height=20, width=80)
+# result_textbox.pack()
+
+# # Run the Tkinter main loop
+# root.mainloop()
+
+
+
+
+# #logic with vectorize and cosine similarity
